6496611f1d930381070685aca66e9829915ba68b
Florian Bruggisser <cansik@users.noreply.github.com>
2020-10-30 19:21:27 +0100

    implemented midas inferencing without good results

--- src/main/java/ch/bildspur/vision/MidasNetwork.java
+++ src/main/java/ch/bildspur/vision/MidasNetwork.java
@@ -3,10 +3,7 @@ package ch.bildspur.vision;
 import ch.bildspur.vision.network.BaseNeuralNetwork;
 import ch.bildspur.vision.result.ImageResult;
 import ch.bildspur.vision.util.CvProcessingUtils;
-import org.bytedeco.opencv.opencv_core.Mat;
-import org.bytedeco.opencv.opencv_core.MatVector;
-import org.bytedeco.opencv.opencv_core.Scalar;
-import org.bytedeco.opencv.opencv_core.StringVector;
+import org.bytedeco.opencv.opencv_core.*;
 import org.bytedeco.opencv.opencv_dnn.Net;
 import processing.core.PImage;
 
@@ -14,11 +11,13 @@ import java.nio.file.Path;
 
 import static org.bytedeco.opencv.global.opencv_core.*;
 import static org.bytedeco.opencv.global.opencv_dnn.*;
+import static org.bytedeco.opencv.global.opencv_imgproc.CV_INTER_AREA;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvResize;
 
 public class MidasNetwork extends BaseNeuralNetwork<ImageResult> {
     private Path model;
     private Net net;
-    private Scalar mean = new Scalar(123.675, 116.28, 103.53, 0.0);
+    private Scalar mean = new Scalar(0.485, 0.456, 0.406, 0.0);
 
     public MidasNetwork(Path model) {
         this.model = model;
@@ -34,8 +33,8 @@ public class MidasNetwork extends BaseNeuralNetwork<ImageResult> {
     public ImageResult run(Mat frame) {
         // convert image into batch of images
         Mat inputBlob = blobFromImage(frame,
-                50.0,
-                frame.size(),
+                1.0,
+                new Size(384, 384),
                 mean, true, false, CV_32F);
 
         // set input
@@ -47,17 +46,16 @@ public class MidasNetwork extends BaseNeuralNetwork<ImageResult> {
 
         // run detection
         net.forward(outs, outNames);
+        Mat output = outs.get(0);
 
-        // extract single result
-        MatVector images = new MatVector();
-        imagesFromBlob(outs.get(0), images);
-        Mat output = images.get(0);
+        // reshape output mat
+        int height = output.size(2);
+        output = output.reshape(1, height);
 
-        output = add(mean, output).asMat();
-        output.convertTo(output, CV_8U);
+        // convert to grayscale image
+        output.convertTo(output, CV_8UC3);
+        //cvResize(output, reshaped, CV_INTER_AREA);
 
-        // convert to processing
-        // todo: make that later (keep free of processing)
         PImage result = new PImage(output.size().width(), output.size().height());
         CvProcessingUtils.toPImage(output, result);
         return new ImageResult(result);
