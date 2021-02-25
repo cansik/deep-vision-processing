03148167e3346b3105ca8833c7e66b963f59c7cc
Florian Bruggisser <cansik@users.noreply.github.com>
2021-02-26 00:53:25 +0100

    implemented first part of the colorization network

---
+++ src/main/java/ch/bildspur/vision/ColorizationNetwork.java
@@ -0,0 +1,105 @@
+package ch.bildspur.vision;
+
+import ch.bildspur.vision.network.BaseNeuralNetwork;
+import ch.bildspur.vision.result.ImageResult;
+import ch.bildspur.vision.util.CvProcessingUtils;
+import org.bytedeco.opencv.global.opencv_dnn;
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_dnn.DictValue;
+import org.bytedeco.opencv.opencv_dnn.Net;
+import processing.core.PImage;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+
+import static org.bytedeco.opencv.global.opencv_core.*;
+import static org.bytedeco.opencv.global.opencv_dnn.*;
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_RGB2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+
+public class ColorizationNetwork extends BaseNeuralNetwork<ImageResult> {
+    private Path protoTextPath;
+    private Path modelPath;
+    private Path clusterCentersPath;
+
+    private Net net;
+    private Scalar mean = new Scalar(-50.0, -50.0, -50.0, 0.0);
+
+    private StringVector outNames;
+
+    public ColorizationNetwork(Path protoTextPath, Path modelPath, Path clusterCentersPath) {
+        this.modelPath = modelPath;
+        this.protoTextPath = protoTextPath;
+        this.clusterCentersPath = clusterCentersPath;
+    }
+
+    @Override
+    public boolean setup() {
+        net = readNetFromCaffe(protoTextPath.toAbsolutePath().toString(), modelPath.toAbsolutePath().toString());
+        outNames = net.getUnconnectedOutLayersNames();
+
+        // setup network
+
+        // read cluster centers (already transposed and reshaped)
+        FileStorage fileStorage = new FileStorage(clusterCentersPath.toAbsolutePath().toString(),
+                FileStorage.READ, StandardCharsets.UTF_8.name());
+        FileNode clusterCentersNode = fileStorage.get("pts_in_hull_prepared");
+        Mat clusterCenters = clusterCentersNode.mat();
+        fileStorage.release();
+
+        // prepare input
+        MatVector inputBlobs = new MatVector();
+        inputBlobs.push_back(clusterCenters);
+        net.getLayer(new DictValue("class8_ab")).blobs(inputBlobs);
+
+        // prepare output
+        MatVector outputBlobs = new MatVector();
+        Mat emptyOutput = new Mat(1, 313, CV_32F);
+        emptyOutput.setTo(new Mat(new Scalar(2.606)));
+        outputBlobs.push_back(emptyOutput);
+        net.getLayer(new DictValue("conv8_313_rh")).blobs(outputBlobs);
+
+        if (DeepVision.ENABLE_CUDA_BACKEND) {
+            net.setPreferableBackend(opencv_dnn.DNN_BACKEND_CUDA);
+            net.setPreferableTarget(opencv_dnn.DNN_TARGET_CUDA);
+        }
+
+        return true;
+    }
+
+    @Override
+    public ImageResult run(Mat frame) {
+        // create grayscale version
+        cvtColor(frame, frame, COLOR_RGB2GRAY);
+
+        // convert image into batch of images
+        Mat inputBlob = blobFromImage(frame,
+                1.0 / 255.0,
+                new Size(224, 224),
+                mean, false, false, CV_32F);
+
+        // set input
+        net.setInput(inputBlob);
+
+        // create output layers
+        // MatVector outs = new MatVector(outNames.size());
+
+        // run detection
+        //net.forward(outs, outNames);
+        Mat out = net.forward();
+
+        // extract single result
+        MatVector images = new MatVector();
+        //imagesFromBlob(outs.get(0), images);
+        Mat output = images.get(0);
+
+        output = add(mean, output).asMat();
+        output.convertTo(output, CV_8U);
+
+        // convert to processing
+        // todo: make that later (keep free of processing)
+        PImage result = new PImage(output.size().width(), output.size().height());
+        CvProcessingUtils.toPImage(output, result);
+        return new ImageResult(result);
+    }
+}
---
+++ src/test/java/ch/bildspur/vision/test/ColorizationTest.java
@@ -0,0 +1,58 @@
+package ch.bildspur.vision.test;
+
+
+import ch.bildspur.vision.ColorizationNetwork;
+import ch.bildspur.vision.DeepVisionPreview;
+import ch.bildspur.vision.StyleTransferNetwork;
+import ch.bildspur.vision.result.ImageResult;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+import java.nio.file.Paths;
+
+public class ColorizationTest extends PApplet {
+
+    public static void main(String... args) {
+        ColorizationTest sketch = new ColorizationTest();
+        sketch.runSketch();
+    }
+
+    public void settings() {
+        size(1280, 480);
+    }
+
+    PImage groundTruth;
+
+    DeepVisionPreview vision = new DeepVisionPreview(this);
+    ColorizationNetwork network;
+    ImageResult result;
+
+    public void setup() {
+        colorMode(HSB, 360, 100, 100);
+
+        groundTruth = loadImage(sketchPath("data/office_bw.jpg"));
+
+        println("creating network...");
+        network = new ColorizationNetwork(
+                Paths.get("networks/colorization_deploy_v2.prototxt"),
+                Paths.get("networks/colorization_release_v2.caffemodel"),
+                Paths.get("networks/colorization_parameter.xml")
+        );
+
+        println("loading model...");
+        network.setup();
+
+        println("inferencing...");
+        result = network.run(groundTruth);
+        println("done!");
+
+        noLoop();
+    }
+
+    public void draw() {
+        background(55);
+        image(groundTruth, 0, 0);
+        image(result.getImage(), 640, 0);
+        surface.setTitle("Style Transfer - FPS: " + Math.round(frameRate));
+    }
+}
